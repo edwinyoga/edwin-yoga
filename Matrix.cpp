1cfa4facfe0c7a3c8563d55aa6dad20ee41f1c6a
Edwin Yoga <44867187+edwinyoga@users.noreply.github.com>
2018-11-08 19:52:15 +0700

    edwinyoga_5170411384

---
+++ Matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    int x[100][100];
+    int m;
+    cout<<"Masukkan Ordo matriks : ";
+    cin>>m;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+            cout<<" Masukan ["<<i<<","<<j<<"] : ";
+            cin>>x[i][j];
+			if(m==i+j+1)
+				x[i][j]=0;
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<m;j++){
+			cout<<" "<<x[i][j];
+		}
+        cout<<endl;
+		}
+system("pause");
+cout<<"Nama : Edwin Yoga Permana "<<endl;
+	cout<<"Nim : 5170411384 ";
+}
